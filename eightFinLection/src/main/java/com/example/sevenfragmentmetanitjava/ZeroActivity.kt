03134a4bf648399722d60722649d4f87da73afff
Smile78 <dronussr@gmail.com>
2021-12-10 06:42:11 +0300

    0х0х7..

---
+++ eightFinLection/src/main/java/com/example/sevenfragmentmetanitjava/ZeroActivity.kt
@@ -0,0 +1,17 @@
+package com.example.sevenfragmentmetanitjava
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class ZeroActivity : AppCompatActivity() {
+
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity1_main)
+
+
+        }
+
+}
